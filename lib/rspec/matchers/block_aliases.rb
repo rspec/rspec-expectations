9151e954218f1b573f630ac3548abf4e640bf4db
Samuel Cochran <sj26@sj26.com>
2012-02-14 12:06:25 +0800

    Block expectations should default to subject block

--- lib/rspec/matchers/block_aliases.rb
+++ lib/rspec/matchers/block_aliases.rb
@@ -13,6 +13,7 @@ module RSpec
     #   expect { this_block }.to change{this.expression}.from(old_value).to(new_value)
     #   expect { this_block }.to raise_error
     def expect(&block)
+      block ||= proc { subject }
       block.extend BlockAliases
     end
   end
