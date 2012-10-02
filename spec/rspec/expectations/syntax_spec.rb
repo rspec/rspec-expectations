f9106dd182abd7487f59222ce648a965b686f33b
Sam Phippen <samphippen@googlemail.com>
2012-10-02 11:26:32 +0100

    Fix specs for the new message string requirement
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- spec/rspec/expectations/syntax_spec.rb
+++ spec/rspec/expectations/syntax_spec.rb
@@ -6,21 +6,21 @@ module RSpec
       describe "the should and should_not expectations" do
         describe "#should" do
           it "raises an error when message isn't a string" do
-            lambda {3.should eq(3), :not_a_string}.should raise_error
+            lambda {3.should eq(3), :not_a_string}.should raise_error /The value passed as the message/
           end
 
           it "doesn't raise an error when message is a string" do
-            lambda {3.should eq(3), "a string"}.should_not raise_error
+            lambda {3.should eq(3), "a string"}.should_not raise_error /The value passed as the message/
           end
         end
 
         describe "#should_not" do
           it "raises an error when message isn't a string" do
-            lambda {3.should_not eq(4), :not_a_string}.should raise_error
+            lambda {3.should_not eq(4), :not_a_string}.should raise_error /The value passed as the message/
           end
 
           it "doesn't raise an error when message is a string" do
-            lambda {3.should_not eq(4), "a string"}.should_not raise_error
+            lambda {3.should_not eq(4), "a string"}.should_not raise_error /The value passed as the message/
           end
         end
       end
