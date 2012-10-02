06643c227bcb0388d7f37e6e24cb15ffe1a73741
Sam Phippen <samphippen@googlemail.com>
2012-10-02 11:32:15 +0100

    Fix message string handling code to pass new specs
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- lib/rspec/expectations/handler.rb
+++ lib/rspec/expectations/handler.rb
@@ -1,24 +1,31 @@
 module RSpec
   module Expectations
-    def message_must_be_string
-      "The value passed as the message for the expectation was not a string"
+
+    class ExpectationHandler
+      def self.message_must_be_string
+        "The value passed as the message for the expectation was not a string"
+      end
+
+      def self.check_message(message=nil)
+        throw message_must_be_string unless message.is_a? String or message == nil
+      end
     end
 
-    class PositiveExpectationHandler        
+    class PositiveExpectationHandler < ExpectationHandler
 
       def self.handle_matcher(actual, matcher, message=nil, &block)
-        raise message_must_be_string unless message.is_a? String or message == nil
+        check_message(message)
         ::RSpec::Matchers.last_should = :should
         ::RSpec::Matchers.last_matcher = matcher
         return ::RSpec::Matchers::BuiltIn::PositiveOperatorMatcher.new(actual) if matcher.nil?
 
         match = matcher.matches?(actual, &block)
         return match if match
-        
+
         message ||= matcher.respond_to?(:failure_message_for_should) ?
                     matcher.failure_message_for_should :
                     matcher.failure_message
-        
+
         if matcher.respond_to?(:diffable?) && matcher.diffable?
           ::RSpec::Expectations.fail_with message, matcher.expected, matcher.actual
         else
@@ -27,18 +34,18 @@ module RSpec
       end
     end
 
-    class NegativeExpectationHandler
+    class NegativeExpectationHandler < ExpectationHandler
       def self.handle_matcher(actual, matcher, message=nil, &block)
-        raise message_must_be_string unless message.is_a? String or message == nil
+        check_message(message)
         ::RSpec::Matchers.last_should = :should_not
         ::RSpec::Matchers.last_matcher = matcher
         return ::RSpec::Matchers::BuiltIn::NegativeOperatorMatcher.new(actual) if matcher.nil?
-        
+
         match = matcher.respond_to?(:does_not_match?) ?
                 !matcher.does_not_match?(actual, &block) :
                 matcher.matches?(actual, &block)
         return match unless match
-        
+
         message ||= matcher.respond_to?(:failure_message_for_should_not) ?
                     matcher.failure_message_for_should_not :
                     matcher.negative_failure_message
