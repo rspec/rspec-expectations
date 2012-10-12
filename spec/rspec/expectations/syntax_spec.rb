385966afa4f42f5be36e5d624c5ae986275a80f1
Sam Phippen <samphippen@googlemail.com>
2012-10-12 17:48:32 +0100

    Make the message string check a warning instead of a raise
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- spec/rspec/expectations/syntax_spec.rb
+++ spec/rspec/expectations/syntax_spec.rb
@@ -6,21 +6,25 @@ module RSpec
       describe "the should and should_not expectations" do
         describe "#should" do
           it "raises an error when the message object isn't a String" do
-            expect {3.should eq(3), :not_a_string}.to raise_error /The value passed as the message/
+            ::Kernel.should_receive(:warn).with /The value passed as the message/
+            3.should eq(3), :not_a_string
           end
 
           it "doesn't raise an error when message is a String" do
-            expect {3.should eq(3), "a string"}.to_not raise_error /The value passed as the message/
+            ::Kernel.should_not_receive(:warn).with /The value passed as the message/
+            3.should eq(3), "a string"
           end
         end
 
         describe "#should_not" do
           it "raises an error when the message object isn't a String" do
-            expect {3.should_not eq(4), :not_a_string}.to raise_error /The value passed as the message/
+            ::Kernel.should_receive(:warn).with /The value passed as the message/
+            3.should_not eq(4), :not_a_string
           end
 
           it "doesn't raise an error when message is a String" do
-            expect {3.should_not eq(4), "a string"}.to_not raise_error /The value passed as the message/
+            ::Kernel.should_not_receive(:warn).with /The value passed as the message/
+            3.should_not eq(4), "a string"
           end
         end
       end
