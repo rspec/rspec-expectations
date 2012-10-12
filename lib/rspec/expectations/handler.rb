6773dfdda56448e7c2226639e5861ada0cc91daa
Sam Phippen <samphippen@googlemail.com>
2012-10-12 10:28:16 +0100

    Flip the order on the conditional for message string/nil checking
    this is done because message will be nil most of the time so we can
    shortcut
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- lib/rspec/expectations/handler.rb
+++ lib/rspec/expectations/handler.rb
@@ -7,7 +7,7 @@ module RSpec
       end
 
       def self.check_message(message=nil)
-        raise message_must_be_string unless String === message or message == nil
+        raise message_must_be_string unless message == nil or String === message
       end
     end
 
