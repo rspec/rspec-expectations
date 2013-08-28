f27d6c380b6f668d618503d8ed11e0c5d2700b89
Myron Marston <myron.marston@gmail.com>
2013-08-28 08:05:17 -0700

    Port the better regex from CallerFilter.

--- lib/rspec/expectations/deprecation.rb
+++ lib/rspec/expectations/deprecation.rb
@@ -1,12 +1,23 @@
 module RSpec
   module Expectations
     module Deprecation
+     RSPEC_LIBS = %w[
+        core
+        mocks
+        expectations
+        matchers
+        rails
+      ]
+
+      ADDITIONAL_TOP_LEVEL_FILES = %w[ autorun ]
+
+      LIB_REGEX = %r{/lib/rspec/(#{(RSPEC_LIBS + ADDITIONAL_TOP_LEVEL_FILES).join('|')})(\.rb|/)}
+
       # @private
       #
       # Used internally to print deprecation warnings
       def deprecate(deprecated, options={})
-        call_site = caller.find { |line| line !~ %r{/lib/rspec/(core|mocks|expectations|matchers|rails)/} }
-        call_site ||= caller(0)[2]
+        call_site = caller.find { |line| line !~ LIB_REGEX }
 
         message = "DEPRECATION: #{deprecated} is deprecated."
         message << " Use #{options[:replacement]} instead." if options[:replacement]
