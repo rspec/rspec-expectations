9a09e0861835f3347c62fbc4b10ee952dfa60915
Jon Rowe <hello@jonrowe.co.uk>
2014-02-08 13:23:57 +1100

    move deprecation helpers and consolidate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,34 +1,5 @@
 Dir['./spec/support/**/*'].each {|f| require f}
 
-module DeprecationHelpers
-  def allow_deprecation
-    allow(RSpec.configuration.reporter).to receive(:deprecation)
-  end
-
-  def expect_no_deprecation
-    expect(RSpec.configuration.reporter).not_to receive(:deprecation)
-  end
-
-  def expect_deprecation_with_call_site(file, line, snippet = //)
-    expect(RSpec.configuration.reporter).to receive(:deprecation) do |options|
-      matcher = include([file, line].join(':'))
-      call_site = options[:call_site] || options[:message]
-
-      unless matcher.matches?(call_site)
-        # RSpec::Expectations::ExpectationNotMetError is rescued in the `match` block
-        # of a custom matcher and returned as `false` from `matches?`. This would
-        # prevent an expectation failure here from surfacing in the test suite if
-        # it's triggered from within a `match` block, so we need to raise
-        # a different error class instead.
-        raise matcher.failure_message_for_should
-      end
-
-      deprecated = options[:deprecated] || options[:message]
-      expect(deprecated).to match(snippet)
-    end
-  end
-end
-
 RSpec::configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.color_enabled = true
@@ -41,7 +12,6 @@ RSpec::configure do |config|
     expectations.syntax = :expect
   end
 
-  config.include SpecHelperMethods
   config.include DeprecationHelpers
 end
 
