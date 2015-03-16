4785ab53568e1506d4d1f2eef904aa14daf91bd6
Nicholas Chmielewski <hackling@gmail.com>
2015-03-16 14:09:34 +1000

    Use ObjectInspector for special case formatting

--- lib/rspec/matchers/built_in/eq.rb
+++ lib/rspec/matchers/built_in/eq.rb
@@ -1,3 +1,5 @@
+RSpec::Support.require_rspec_support('object_inspector')
+
 module RSpec
   module Matchers
     module BuiltIn
@@ -36,38 +38,7 @@ module RSpec
         end
 
         def format_object(object)
-          if Time === object
-            format_time(object)
-          elsif defined?(DateTime) && DateTime === object
-            format_date_time(object)
-          elsif defined?(BigDecimal) && BigDecimal === object
-            "#{object.to_s 'F'} (#{object.inspect})"
-          else
-            object.inspect
-          end
-        end
-
-        TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
-
-        if Time.method_defined?(:nsec)
-          def format_time(time)
-            time.strftime("#{TIME_FORMAT}.#{"%09d" % time.nsec} %z")
-          end
-        else # for 1.8.7
-          def format_time(time)
-            time.strftime("#{TIME_FORMAT}.#{"%06d" % time.usec} %z")
-          end
-        end
-
-        DATE_TIME_FORMAT = "%a, %d %b %Y %H:%M:%S.%N %z"
-        # ActiveSupport sometimes overrides inspect. If `ActiveSupport` is
-        # defined use a custom format string that includes more time precision.
-        def format_date_time(date_time)
-          if defined?(ActiveSupport)
-            date_time.strftime(DATE_TIME_FORMAT)
-          else
-            date_time.inspect
-          end
+          RSpec::Support::ObjectInspector.inspect(object)
         end
       end
     end
