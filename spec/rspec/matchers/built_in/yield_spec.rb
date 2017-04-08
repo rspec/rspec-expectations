rspec/rspec-metagem

commit e7ffd019333f671b7d4f2181bf567adb5fc5a63b

yield_with_args: match immediately (#965)
* yield_with_args: match immediately

This change makes `yield_with_args(matcher)` evaluate the matcher as
soon as the arguments are yielded, rather than after the block has run.

This is more correct behaviour, as it allows testing of methods that
yield a value before making some change to it. Currently, there is no
way to use a yield matcher to test that a block yields a value with some
predicate being false, then returns with that predicate being true.

* yield_*: don't capture ivar in proc

* yield_*: default value for Probe callback

* yield_*: rename confusingly-named methods

* yield_with_args: test matching is immediate

* yield_successively: apply matchers immediately

* yield_sucessively: fix tests on Ruby 1.8.7

* yield_successively: use Proc.new instead of proc

`proc`'s behaviour changes between 1.8.x and later version of Ruby.

* yield_successively: prefer Array#size over #count

See
https://github.com/rspec/rspec-expectations/pull/965#discussion_r98151802.

* yield_successive_args: simplify real-time matching

Suggested in
https://github.com/rspec/rspec-expectations/pull/965#discussion_r98151938.

* yield_with_args: use YieldProbe#single_yield_args

See
https://github.com/rspec/rspec-expectations/pull/965#discussion_r98149521.

* tests: invert incorrect yield spec names

See
https://github.com/rspec/rspec-expectations/pull/965#discussion_r97075553.

* tests: use a more obvious way to clear strings

See
https://github.com/rspec/rspec-expectations/pull/965#discussion_r98152777.

* tests: fix always-passing yield test

See
https://github.com/rspec/rspec-expectations/pull/965#discussion_r98153225.

* tests: fix other always-passing yield test

See
https://github.com/rspec/rspec-expectations/pull/965#discussion_r98153371.

* yield_with_args: remove unused method

See
https://github.com/rspec/rspec-expectations/pull/965#discussion_r99469388.

lib/rspec/matchers/built_in/yield.rb
@@ -8,19 +8,17 @@ module RSpec
       # yield matchers is used. Provides information about
       # the yield behavior of the object-under-test.
       class YieldProbe
-        def self.probe(block)
-          probe = new(block)
+        def self.probe(block, &callback)
+          probe = new(block, &callback)
           return probe unless probe.has_block?
-          probe.assert_valid_expect_block!
-          block.call(probe)
-          probe.assert_used!
-          probe
+          probe.probe
         end
 
         attr_accessor :num_yields, :yielded_args
 
-        def initialize(block)
+        def initialize(block, &callback)
           @block = block
+          @callback = callback || Proc.new {}
           @used = false
           self.num_yields = 0
           self.yielded_args = []
@@ -30,13 +28,22 @@ module RSpec
           Proc === @block
         end
 
+        def probe
+          assert_valid_expect_block!
+          @block.call(self)
+          assert_used!
+          self
+        end
+
         def to_proc
           @used = true
 
           probe = self
+          callback = @callback
           Proc.new do |*args|
             probe.num_yields += 1
             probe.yielded_args << args
+            callback.call(*args)
             nil # to indicate the block does not return a meaningful value
           end
         end
@@ -269,10 +276,15 @@ module RSpec
 
         # @private
         def matches?(block)
-          @probe = YieldProbe.probe(block)
+          @args_matched_when_yielded = true
+          @probe = YieldProbe.new(block) do
+            @actual = @probe.single_yield_args
+            @actual_formatted = actual_formatted
+            @args_matched_when_yielded &&= args_currently_match?
+          end
           return false unless @probe.has_block?
-          @actual = @probe.single_yield_args
-          @probe.yielded_once?(:yield_with_args) && args_match?
+          @probe.probe
+          @probe.yielded_once?(:yield_with_args) && @args_matched_when_yielded
         end
 
         # @private
@@ -317,16 +329,16 @@ module RSpec
         def negative_failure_reason
           if !@probe.has_block?
             'was not a block'
-          elsif all_args_match?
+          elsif @args_matched_when_yielded && !@expected.empty?
             'yielded with expected arguments' \
               "\nexpected not: #{surface_descriptions_in(@expected).inspect}" \
-              "\n         got: #{actual_formatted}"
+              "\n         got: #{@actual_formatted}"
           else
             'did'
           end
         end
 
-        def args_match?
+        def args_currently_match?
           if @expected.empty? # expect {...}.to yield_with_args
             @positive_args_failure = 'yielded with no arguments' if @actual.empty?
             return !@actual.empty?
@@ -335,7 +347,7 @@ module RSpec
           unless (match = all_args_match?)
             @positive_args_failure = 'yielded with unexpected arguments' \
               "\nexpected: #{surface_descriptions_in(@expected).inspect}" \
-              "\n     got: #{actual_formatted}"
+              "\n     got: #{@actual_formatted}"
           end
 
           match
@@ -356,10 +368,19 @@ module RSpec
 
         # @private
         def matches?(block)
-          @probe = YieldProbe.probe(block)
-          return false unless @probe.has_block?
+          @actual_formatted = []
+          args_matched_when_yielded = true
+          yield_count = 0
+
+          @probe = YieldProbe.probe(block) do |args|
+            @actual_formatted << RSpec::Support::ObjectFormatter.format(args)
+            args_matched_when_yielded &&= values_match?(@expected[yield_count], args)
+            yield_count += 1
+          end
+
           @actual = @probe.successive_yield_args
-          args_match?
+          return false unless @probe.has_block?
+          args_matched_when_yielded && yield_count == @expected.length
         end
 
         def does_not_match?(block)
@@ -390,10 +411,6 @@ module RSpec
 
       private
 
-        def args_match?
-          values_match?(@expected, @actual)
-        end
-
         def expected_arg_description
           @expected.map { |e| description_of e }.join(', ')
         end
@@ -403,7 +420,7 @@ module RSpec
 
           'yielded with unexpected arguments' \
           "\nexpected: #{surface_descriptions_in(@expected).inspect}" \
-          "\n     got: #{actual_formatted}"
+          "\n     got: [#{@actual_formatted.join(", ")}]"
         end
 
         def negative_failure_reason
@@ -411,7 +428,7 @@ module RSpec
 
           'yielded with expected arguments' \
           "\nexpected not: #{surface_descriptions_in(@expected).inspect}" \
-          "\n         got: #{actual_formatted}"
+          "\n         got: [#{@actual_formatted.join(", ")}]"
         end
       end
     end

spec/rspec/matchers/built_in/yield_spec.rb
@@ -309,6 +309,14 @@ RSpec.describe "yield_with_args matcher" do
       expect { |b| _yield_with_args(1, &b) }.to yield_with_args
     end
 
+    it 'passes if the matchers match at yield time only' do
+      expect { |b|
+        val = []
+        _yield_with_args(val, &b)
+        val << 1
+      }.to yield_with_args(be_empty)
+    end
+
     it 'fails if the block does not yield' do
       expect {
         expect { |b| _dont_yield(&b) }.to yield_with_args
@@ -319,6 +327,20 @@ RSpec.describe "yield_with_args matcher" do
       expect {
         expect { |b| _yield_with_no_args(&b) }.to yield_with_args
       }.to fail_with(/expected given block to yield with arguments, but yielded with no arguments/)
+    end
+
+    it 'fails if the matchers match at return time only' do
+      expect {
+        expect { |b|
+          val = [1]
+          _yield_with_args(val, &b)
+          val.clear
+        }.to yield_with_args(be_empty)
+      }.to fail_with(dedent <<-EOS)
+        |expected given block to yield with arguments, but yielded with unexpected arguments
+        |expected: [(be empty)]
+        |     got: [[1]]
+      EOS
     end
 
     it 'raises an error if it yields multiple times' do
@@ -335,12 +357,34 @@ RSpec.describe "yield_with_args matcher" do
       }.to fail_with(/expected given block not to yield with arguments, but did/)
     end
 
+    it 'fails if the matchers match at yield time only' do
+      expect {
+        expect { |b|
+          val = []
+          _yield_with_args(val, &b)
+          val << 1
+        }.not_to yield_with_args(be_empty)
+      }.to fail_with(dedent <<-EOS)
+        |expected given block not to yield with arguments, but yielded with expected arguments
+        |expected not: [(be empty)]
+        |         got: [[]]
+      EOS
+    end
+
     it 'passes if the block does not yield' do
       expect { |b| _dont_yield(&b) }.not_to yield_with_args
     end
 
     it 'passes if the block yields with no arguments' do
       expect { |b| _yield_with_no_args(&b) }.not_to yield_with_args
+    end
+
+    it 'passes if the matchers match at return time only' do
+      expect { |b|
+        val = [1]
+        _yield_with_args(val, &b)
+        val.clear
+      }.not_to yield_with_args(be_empty)
     end
 
     it 'fails if the expect block does not accept an argument', :if => (RUBY_VERSION.to_f > 1.8) do
@@ -504,7 +548,7 @@ RSpec.describe "yield_successive_args matcher" do
 
   it_behaves_like "an RSpec matcher",
       :valid_value => lambda { |b| [1, 2].each(&b) },
-      :invalid_value => lambda { |b| _dont_yield(&b) } do
+      :invalid_value => lambda { |b| [3, 4].each(&b) } do
     let(:matcher) { yield_successive_args(1, 2) }
   end
 
@@ -524,6 +568,15 @@ RSpec.describe "yield_successive_args matcher" do
       expect { |b| [ [:a, 1], [:b, 2] ].each(&b) }.to yield_successive_args([:a, 1], [:b, 2])
     end
 
+    it 'passes if matched at yield time only' do
+      expect { |b|
+        [ [:a, 1], [:b, 2] ].each do |val|
+          _yield_with_args(val, &b)
+          val.clear
+        end
+      }.to yield_successive_args([:a, 1], [:b, 2])
+    end
+
     it 'fails when the block does not yield that many times' do
       expect {
         expect { |b| [[:a, 1]].each(&b) }.to yield_successive_args([:a, 1], [:b, 2])
@@ -534,6 +587,22 @@ RSpec.describe "yield_successive_args matcher" do
       expect {
         expect { |b| [ [:a, 1], [:b, 3] ].each(&b) }.to yield_successive_args([:a, 1], [:b, 2])
       }.to fail_with(/but yielded with unexpected arguments/)
+    end
+
+    it 'fails if matched at return time only' do
+      expect {
+        expect { |b|
+          [ [:a, 1], [:b, 2] ].each do |eventual|
+            initial = []
+            _yield_with_args(initial, &b)
+            initial.concat(eventual)
+          end
+        }.to yield_successive_args([:a, 1], [:b, 2])
+      }.to fail_with(dedent <<-EOS)
+        |expected given block to yield successively with arguments, but yielded with unexpected arguments
+        |expected: [[:a, 1], [:b, 2]]
+        |     got: [[], []]
+      EOS
     end
   end
 
@@ -611,16 +680,41 @@ RSpec.describe "yield_successive_args matcher" do
   end
 
   describe "expect {...}.not_to yield_successive_args(matcher, matcher)" do
-    it 'passes when the successively yielded args match the matchers' do
+    it 'passes when the successively yielded args do not match the matchers' do
       expect { |b|
         %w[ barn food ].each(&b)
       }.not_to yield_successive_args(a_string_matching(/foo/), a_string_matching(/bar/))
     end
 
-    it 'fails when the successively yielded args do not match the matchers' do
+    it 'passes when the successively yielded args do not match the matchers (at yield time only)' do
+      expect { |b|
+        %w[ food barn ].each do |eventual|
+          initial = String.new
+          _yield_with_args(initial, &b)
+          initial << eventual
+        end
+      }.not_to yield_successive_args(a_string_matching(/foo/), a_string_matching(/bar/))
+    end
+
+    it 'fails when the successively yielded args match the matchers' do
       expect {
         expect { |b|
           %w[ food barn ].each(&b)
+        }.not_to yield_successive_args(a_string_matching(/foo/), a_string_matching(/bar/))
+      }.to fail_with(dedent <<-EOS)
+        |expected given block not to yield successively with arguments, but yielded with expected arguments
+        |expected not: [(a string matching /foo/), (a string matching /bar/)]
+        |         got: ["food", "barn"]
+      EOS
+    end
+
+    it 'fails when the successively yielded args match the matchers (at yield time only)' do
+      expect {
+        expect { |b|
+          %w[ food barn ].each do |val|
+            _yield_with_args(val, &b)
+            val.sub!(/.+/, '')
+          end
         }.not_to yield_successive_args(a_string_matching(/foo/), a_string_matching(/bar/))
       }.to fail_with(dedent <<-EOS)
         |expected given block not to yield successively with arguments, but yielded with expected arguments

rspec/rspec-expectations

commit 9a6f6c870cd6b01946d244728eb1f6e3b73b587c

Fix regression introduced in #965.

Gemfile
@@ -12,6 +12,7 @@ branch = File.read(File.expand_path("../maintenance-branch", __FILE__)).chomp
   end
 end
 
+gem 'coderay' # for syntax highlighting
 gem 'yard', '0.8.7.4', :require => false
 
 ### deps for rdoc.info

lib/rspec/matchers/built_in/yield.rb
@@ -63,12 +63,6 @@ module RSpec
           end
         end
 
-        def successive_yield_args
-          yielded_args.map do |arg_array|
-            arg_array.size == 1 ? arg_array.first : arg_array
-          end
-        end
-
         def assert_used!
           return if @used
           raise 'You must pass the argument yielded to your expect block on ' \
@@ -369,16 +363,18 @@ module RSpec
         # @private
         def matches?(block)
           @actual_formatted = []
+          @actual = []
           args_matched_when_yielded = true
           yield_count = 0
 
-          @probe = YieldProbe.probe(block) do |args|
-            @actual_formatted << RSpec::Support::ObjectFormatter.format(args)
-            args_matched_when_yielded &&= values_match?(@expected[yield_count], args)
+          @probe = YieldProbe.probe(block) do |*arg_array|
+            arg_or_args = arg_array.size == 1 ? arg_array.first : arg_array
+            @actual_formatted << RSpec::Support::ObjectFormatter.format(arg_or_args)
+            @actual << arg_or_args
+            args_matched_when_yielded &&= values_match?(@expected[yield_count], arg_or_args)
             yield_count += 1
           end
 
-          @actual = @probe.successive_yield_args
           return false unless @probe.has_block?
           args_matched_when_yielded && yield_count == @expected.length
         end

spec/rspec/matchers/built_in/yield_spec.rb
@@ -563,6 +563,30 @@ RSpec.describe "yield_successive_args matcher" do
     expect(val).to be_nil
   end
 
+  it "works correctly when the method yield multiple args each time (passing case)" do
+    expect { |block|
+      ['football', 'barstool'].each_with_index(&block)
+    }.to yield_successive_args(
+      [/foo/,                         0],
+      [a_string_starting_with('bar'), 1]
+    )
+  end
+
+  it "works correctly when the method yield multiple args each time (failing case)" do
+    expect {
+      expect { |block|
+        ['football', 'barstool'].each_with_index(&block)
+      }.to yield_successive_args(
+        [/foo/,                         0],
+        [a_string_starting_with('abr'), 1]
+      )
+    }.to fail_with(dedent <<-EOS)
+      |expected given block to yield successively with arguments, but yielded with unexpected arguments
+      |expected: [[/foo/, 0], [(a string starting with "abr"), 1]]
+      |     got: [["football", 0], ["barstool", 1]]
+    EOS
+  end
+
   describe "expect {...}.to yield_successive_args([:a, 1], [:b, 2])" do
     it 'passes when the block successively yields the given args' do
       expect { |b| [ [:a, 1], [:b, 2] ].each(&b) }.to yield_successive_args([:a, 1], [:b, 2])