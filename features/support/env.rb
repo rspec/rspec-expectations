ad96b8d5943f058530f7542f1bf188e9e9ec363e
Phil Pirozhkov <pirjsuka@gmail.com>
2019-02-12 17:28:27 +0700

    Make features global configuration proof
    If a `~/.config/rspec/options` file contains the following:
    
        --color
        --profile 2
        --format progress
        --require pry
    
    a number of features fail, since options are picked up and used by the
    code under test.
    
    Setting `XDG_CONFIG_HOME` to a directory that does exist skips loading
    the global configuration.

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,6 +10,12 @@ end
 
 Aruba.configure do |config|
   config.before_cmd do |cmd|
+    set_env('XDG_CONFIG_HOME', 'nonexistent-directory') # skip loading global configuration
+  end
+end
+
+Aruba.configure do |config|
+  config.before_cmd do |cmd|
     set_env('JRUBY_OPTS', "-X-C #{ENV['JRUBY_OPTS']}") # disable JIT since these processes are so short lived
   end
 end if RUBY_PLATFORM == 'java'
