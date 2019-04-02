eebad3fcfbbf0139a39c8c40b4c4cd1cf91d1589
taichi <taichi730@gmail.com>
2019-04-03 00:56:08 +0900

    removed #error_message method and give the error message inline

--- lib/rspec/matchers/built_in/be_instance_of.rb
+++ lib/rspec/matchers/built_in/be_instance_of.rb
@@ -17,18 +17,14 @@ module RSpec
           if actual_object_respond_to?(actual, :instance_of?)
             actual.instance_of?(expected)
           else
-            raise ::ArgumentError, error_message
+            raise ::ArgumentError, "The #{matcher_name} matcher requires that " \
+                                   "the actual object responds to #instance_of? method " \
+                                   "but it does not respond to the method."
           end
         end
 
         def actual_object_respond_to?(actual, method)
           ::Kernel.instance_method(:respond_to?).bind(actual).call(method)
-        end
-
-        def error_message
-          "The #{matcher_name} matcher requires that " \
-          "the actual object responds to #instance_of? method " \
-          "but it does not respond to the method."
         end
       end
     end
--- lib/rspec/matchers/built_in/be_kind_of.rb
+++ lib/rspec/matchers/built_in/be_kind_of.rb
@@ -13,18 +13,14 @@ module RSpec
           elsif actual_object_respond_to?(actual, :is_a?)
             actual.is_a?(expected)
           else
-            raise ::ArgumentError, error_message
+            raise ::ArgumentError, "The #{matcher_name} matcher requires that " \
+                                   "the actual object responds to either #kind_of? or #is_a? methods "\
+                                   "but it responds to neigher of two methods."
           end
         end
 
         def actual_object_respond_to?(actual, method)
           ::Kernel.instance_method(:respond_to?).bind(actual).call(method)
-        end
-
-        def error_message
-          "The #{matcher_name} matcher requires that the actual object " \
-          "responds to either #kind_of? or #is_a? methods but " \
-          "it responds to neigher of two methods."
         end
       end
     end
