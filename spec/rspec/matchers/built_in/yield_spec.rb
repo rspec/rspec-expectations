96bad969dc56e75b64661bc29f01207b46aaa30c
Marc-André Lafortune <github@marc-andre.ca>
2020-03-18 15:37:54 -0400

    yield_control hardening (#1167)
    Prevent invalid argument given to yield_control.exactly / at_least / at_most
    
    Previously `exactly(:trice)` would always provide a failure with message 'expected given block to yield control', even if control was yielded.
    
    `at_least(:trice)` yields a cryptic error 'ArgumentError: comparison of Integer with nil failedinstead of an error as it should'.

--- spec/rspec/matchers/built_in/yield_spec.rb
+++ spec/rspec/matchers/built_in/yield_spec.rb
@@ -58,13 +58,29 @@ RSpec.describe "yield_control matcher" do
     it 'fails if the block does not yield' do
       expect {
         expect { |b| _dont_yield(&b) }.to yield_control
-      }.to fail_with(/expected given block to yield control/)
+      }.to fail_with(/expected given block to yield control but/)
     end
 
     it 'does not return a meaningful value from the block' do
       val = nil
       expect { |b| val = _yield_with_args(&b) }.to yield_control
       expect(val).to be_nil
+    end
+
+    it 'raises an error if given an invalid count argument' do
+      expect { yield_control.exactly('2') }.to raise_error(ArgumentError)
+      expect { yield_control.at_least(:trice_with_typo) }.to raise_error(ArgumentError)
+      expect { yield_control.at_most(nil) }.to raise_error(ArgumentError)
+    end
+
+    it 'is yet to support multiple calls to compatible count constraints' do
+      pending
+      expect { |b| 1.upto(4, &b) }.to yield_control.at_least(3).at_most(4).times
+      expect { |b| 1.upto(2, &b) }.not_to yield_control.at_least(3).at_most(4).times
+    end
+
+    it 'raises an error on multiple incompatible calls to count constraints' do
+      expect { yield_control.once.twice }.to raise_error(/multiple/i)
     end
 
     context "with exact count" do
