d4709bd3e8f7119ecc2b390183654ab9f750c7fc
Phil Pirozhkov <hello@fili.pp.ru>
2020-04-03 18:24:23 +0300

    Updated travis build scripts (from rspec-dev)

--- script/functions.sh
+++ script/functions.sh
@@ -1,4 +1,4 @@
-# This file was generated on 2020-03-30T16:31:25+02:00 from the rspec-dev repo.
+# This file was generated on 2020-04-03T18:24:23+03:00 from the rspec-dev repo.
 # DO NOT modify it by hand as your changes will get lost the next time it is generated.
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
