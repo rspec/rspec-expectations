85cbfa6303b80e35dfcceb2aec8f68a5e17cfc16
Jon Rowe <hello@jonrowe.co.uk>
2020-11-17 08:49:44 +0000

    Merge pull request #1227 from rspec/update-ci-build-scripts-2020-11-16-for-main
    Updates from rspec-dev (2020-11-16)

--- script/ci_functions.sh
+++ script/ci_functions.sh
@@ -1,4 +1,4 @@
-# This file was generated on 2020-11-16T09:31:10+00:00 from the rspec-dev repo.
+# This file was generated on 2020-11-16T22:10:30+00:00 from the rspec-dev repo.
 # DO NOT modify it by hand as your changes will get lost the next time it is generated.
 
 # Taken from:
