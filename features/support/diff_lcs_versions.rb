bc763f06ba23c57bcc65ce9cabea7e0c083d2398
Jon Rowe <hello@jonrowe.co.uk>
2020-12-06 11:52:14 +0000

    Skip scenario on diff-lcs 1.4.3

--- features/support/diff_lcs_versions.rb
+++ features/support/diff_lcs_versions.rb
@@ -8,6 +8,14 @@ Around "@skip-when-diff-lcs-1.4" do |scenario, block|
   end
 end
 
+Around "@skip-when-diff-lcs-1.4.3" do |scenario, block|
+  if Diff::LCS::VERSION =~ /1\.4\.3/
+    warn "Skipping scenario #{scenario.title} on `diff-lcs` v#{Diff::LCS::VERSION}"
+  else
+    block.call
+  end
+end
+
 Around "@skip-when-diff-lcs-1.3" do |scenario, block|
   if Diff::LCS::VERSION.to_f < 1.4
     warn "Skipping scenario #{scenario.title} on `diff-lcs` v#{Diff::LCS::VERSION.to_f}"
