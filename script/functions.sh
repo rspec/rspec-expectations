6de8f5401f7df50d2a92fcc92af05fbafd2b8366
Jon Rowe <hello@jonrowe.co.uk>
2020-12-08 22:34:46 +0000

    Merge pull request #1236 from rspec/update-ci-build-scripts-2020-12-08-for-main
    Updates from rspec-dev (2020-12-08)

--- script/functions.sh
+++ script/functions.sh
@@ -1,4 +1,4 @@
-# This file was generated on 2020-11-22T07:41:13+00:00 from the rspec-dev repo.
+# This file was generated on 2020-12-08T21:16:17+00:00 from the rspec-dev repo.
 # DO NOT modify it by hand as your changes will get lost the next time it is generated.
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -88,7 +88,10 @@ function run_spec_suite_for {
       echo "Running specs for $1"
       pushd ../$1
       unset BUNDLE_GEMFILE
-      bundle_install_flags=`cat .travis.yml | grep bundler_args | tr -d '"' | grep -o " .*"`
+      bundle_install_flags=""
+      if [ -e .travis.yml ]; then
+        bundle_install_flags=`cat .travis.yml | grep bundler_args | tr -d '"' | grep -o " .*"`
+      fi
       travis_retry eval "(unset RUBYOPT; exec bundle install $bundle_install_flags)"
       run_specs_and_record_done
       popd
