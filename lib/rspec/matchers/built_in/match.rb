962ccf750cbc582a93fd512327c9b5ddd420da54
Phil Pirozhkov <hello@fili.pp.ru>
2020-12-12 15:17:30 +0300

    Remove redundant wrapper

--- lib/rspec/matchers/built_in/match.rb
+++ lib/rspec/matchers/built_in/match.rb
@@ -52,7 +52,6 @@ module RSpec
         def match_captures(expected, actual)
           match = actual.match(expected)
           if match
-            match = MatchData.new(match)
             if match.names.empty?
               values_match?(@expected_captures, match.captures)
             else
@@ -65,31 +64,6 @@ module RSpec
             false
           end
         end
-      end
-
-      # @private
-      class MatchData
-        def initialize(match_data)
-          @match_data = match_data
-        end
-
-        # @api private
-        # Returns match data names for named captures
-        # @return Array
-        def names
-          match_data.names
-        end
-
-        # @api private
-        # returns an array of captures from the match data
-        # @return Array
-        def captures
-          match_data.captures
-        end
-
-      protected
-
-        attr_reader :match_data
       end
     end
   end
