05a964bb57f95463fa2fde3425c9cc65c8365aad
Benoit Tigeot <benoit@hopsandfork.com>
2020-12-21 20:43:17 +0100

    Updated ci build scripts (from rspec-dev)

--- script/predicate_functions.sh
+++ script/predicate_functions.sh
@@ -1,4 +1,4 @@
-# This file was generated on 2020-12-09T08:28:47+00:00 from the rspec-dev repo.
+# This file was generated on 2020-12-21T20:43:17+01:00 from the rspec-dev repo.
 # DO NOT modify it by hand as your changes will get lost the next time it is generated.
 
 function is_mri {
