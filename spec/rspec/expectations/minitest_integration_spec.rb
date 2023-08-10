60b17cf66d0f98ad4c1680eeb5585f6862434696
Jon Rowe <hello@jonrowe.co.uk>
2023-08-10 21:25:36 +0100

    Merge pull request #1427 from rspec/fix-minitest-spec
    Change MiniTest to Minitest

--- spec/rspec/expectations/minitest_integration_spec.rb
+++ spec/rspec/expectations/minitest_integration_spec.rb
@@ -12,7 +12,7 @@ module RSpec
 
       it "includes itself in Minitest::Test, and sets up our exceptions to be counted as assertion failures" do
         with_minitest_loaded do
-          minitest_case = MiniTest::Test.allocate
+          minitest_case = ::Minitest::Test.allocate
           expect(minitest_case).to respond_to(*sample_matchers)
 
           expect(RSpec::Expectations::ExpectationNotMetError).to be ::Minitest::Assertion
