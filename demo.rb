f6d9de0d053906725c309b432d4c93917ea3a3fd
Jon Rowe <hello@jonrowe.co.uk>
2024-02-23 21:42:58 +0000

    Remove accidentally commited file

--- demo.rb
+++
@@ -1,9 +0,0 @@
-require './lib/rspec/expectations'
-class IsolatedTest
-  include RSpec::Matchers
-  def test
-    expect(nil).to(eq(""))
-  end
-end
-RSpec::Expectations::Syntax.enable_expect(IsolatedTest)
-IsolatedTest.new.test
